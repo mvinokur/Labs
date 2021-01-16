ff7e3d26899cb7bdbcf9e8c40f817ddc39ad9335
CRooZZEERR <50952210+CRooZZEERR@users.noreply.github.com>
2021-01-16 23:10:15 +0300

    Some addings

--- GameDB-2-3/src/sql/game/db/Main.java
+++ GameDB-2-3/src/sql/game/db/Main.java
@@ -12,19 +12,26 @@ public class Main {
         base.connect();
         PlatformTable b_tab = base.getPlatformTable();
         GameTable s_tab = base.getGameTable();
+        System.out.println(" 3 = PC, 2 = Xbox, 1 = PS\n");
+        //вывод всего
         for(Game item : s_tab.selectAll()){
-            System.out.println("("+item.platform_id+") "+item.title+"  "+item.price+"  "+item.release_date);
+            System.out.println(" "+item.platform_id +" "+ item.title+"  "+item.price+"  "+item.release_date);
         }
         System.out.println();
+        /*
+        //вывод по платформе
         for(Game item : s_tab.selectPlatform("2")){
             System.out.println("("+item.platform_id+") "+item.title+"  "+item.price+"  "+item.release_date);
         }
         System.out.println();
+        //вывод по деньгам
         for(Game item : s_tab.selectPrice(60, 150)){
             System.out.println("("+item.platform_id+") "+item.title+"  "+item.price+"  "+item.release_date);
         }
+         */
         System.out.println();
         base.joinSelect();
+
     }
 
 }
